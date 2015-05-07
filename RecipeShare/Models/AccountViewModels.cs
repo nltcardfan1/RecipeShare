9d860ae028a279cec5019430abc99451a26164fd
nltcardfan1 <nltcardfan1@gmail.com>
2015-05-07 17:17:14 -0400

    added first and last name to register form

--- RecipeShare/Models/AccountViewModels.cs
+++ RecipeShare/Models/AccountViewModels.cs
@@ -69,6 +69,16 @@ namespace RecipeShare.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+		[Required]
+        [Display(Name = "First Name")]
+		[StringLength(50)]
+        public string FirstName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
